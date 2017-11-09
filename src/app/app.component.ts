448111caf562022962a17d6464d281e2c0594ac5
Hernan Y.Ke <yuriqiao@gmail.com>
2017-11-08 19:39:57 -0500

    npm install --save @angular/material @angular/cdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
+  providers:[
+    ApiService
+  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
+  constructor(private apiService: ApiService){}
+  ngOnInit(){
+    this.apiService.getMessage();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpModule} from '@angular/http';
+import {MatButtonModule} from '@angular/material/button';
 import { AppComponent } from './app.component';
-
+import { ApiService} from './api.service';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
