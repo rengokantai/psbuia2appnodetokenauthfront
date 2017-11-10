c26f13ef0c3d14537f3d29420475aeccea37988d
Hernan Y.Ke <yuriqiao@gmail.com>
2017-11-10 17:28:35 -0500

    updated UserModel

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component,OnInit } from '@angular/core';
   template: `
   <mat-toolbar>
   YK
+    <button mat-button routerLink="/users">Users</button>
   <span style="flex: 1 1 auto"></span>
   <button mat-button routerLink="register">Register</button>
   </mat-toolbar>
--- src/app/register.component.ts
+++ src/app/register.component.ts
@@ -6,26 +6,7 @@ import {AuthService} from './auth.service';
     AuthService
   ],
   selector: 'register',
-  template: `Register
-  <mat-card>
-  <mat-card-header>
-  <mat-card-title>
-    <h4>Register new user</h4>
-  </mat-card-title>
-  </mat-card-header>
-  <mat-card-content>
-  <form>
-  <mat-input-container>
-    <input [(ngModel)]="registerData.email" name="email" matInput placeholder="email" type="email">
-  </mat-input-container>
-  <mat-input-container>
-    <input [(ngModel)]="registerData.pwd" name="password" matInput placeholder="password" type="password">
-  </mat-input-container>
-  <button (click)="post()" mat-raised-button color="primary">register</button>
-  </form>
-  </mat-card-content>
-  </mat-card>
-  `
+  templateUrl:'register.component.html'
 })
 export class RegisterComponent {
   constructor(private authService: AuthService){}
